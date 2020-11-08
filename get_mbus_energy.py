1408960ceb82595e101639c963ac07788a461a54
christof1977 <github@burningwheels.com>
2020-11-08 11:58:17 +0100

    Beautified get_mbus_energy a little bit, adapted to new class name Mysqldose

--- get_mbus_energy.py
+++ get_mbus_energy.py
@@ -4,16 +4,13 @@
 import time
 import os
 import json
+import logging
 from libby import mbus
 from libby import mysqldose
-from libby.logger import logger 
 import configparser
 
 configfile = 'collectdata.ini'
-#logging = False
-logging = True
-logdata = True
-
+logging.basicConfig(level=logging.INFO)
     
 if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(__file__))
@@ -28,47 +25,33 @@ if __name__ == '__main__':
         mysqldb = config['BASE']['Mysqldb']
 
     except Exception as e:
-        logger("Configuration error", logging)
-        logger(str(e), logging)
+        logging.error("Configuration error", str(e))
 
+    mb = mbus.mbus()
+    db = mysqldose.Mysqldose(mysqluser, mysqlpass, mysqlserv, mysqldb)
 
-    #try:
-    if True:
-        mb = mbus.mbus()
-        db = mysqldose.mysqldose(mysqluser, mysqlpass, mysqlserv, mysqldb)
+    result = mb.do_char_dev()
+    energy = []
 
-        result = mb.do_char_dev()
-        energy = []
+    job = json.loads(result)
+    for i in (job['body']['records']):
+        if i['type'] == 'VIFUnit.ENERGY_WH':
+            energy.append(float(i['value']))
+        if i['type'] == 'VIFUnit.FLOW_TEMPERATURE':
+            flow_temp = float(i['value'])
+        if i['type'] == 'VIFUnit.RETURN_TEMPERATURE':
+            return_temp =float(i['value'])
+        if i['type'] == 'VIFUnit.POWER_W' and i['function'] == 'FunctionType.INSTANTANEOUS_VALUE':
+            power = float(i['value'])
+        if i['type'] == 'VIFUnit.VOLUME_FLOW' and i['function'] == 'FunctionType.INSTANTANEOUS_VALUE':
+            flow = float(i['value'])
+    data =[]
+    data.append("Verbrauch: " + str(max(energy)/1e6) + " MWh")
+    data.append("Vorlauf: " + str(flow_temp) + "°C")
+    data.append("Rücklauf: " + str(return_temp) + "°C")
+    data.append("Leistung: " + str(power) + " W")
+    data.append("Durchfluss: " + str(round(flow*1000,2)) + " l/h")
 
-        job = json.loads(result)
-        for i in (job['body']['records']):
-            if i['type'] == 'VIFUnit.ENERGY_WH':
-                energy.append(float(i['value']))
-            if i['type'] == 'VIFUnit.FLOW_TEMPERATURE':
-                flow_temp = float(i['value'])
-            if i['type'] == 'VIFUnit.RETURN_TEMPERATURE':
-                return_temp =float(i['value'])
-            if i['type'] == 'VIFUnit.POWER_W' and i['function'] == 'FunctionType.INSTANTANEOUS_VALUE':
-                power = float(i['value'])
-            if i['type'] == 'VIFUnit.VOLUME_FLOW' and i['function'] == 'FunctionType.INSTANTANEOUS_VALUE':
-                flow = float(i['value'])
-
-
-        data =[]
-        data.append("Verbrauch: " + str(max(energy)/1e6) + " MWh")
-        data.append("Vorlauf: " + str(flow_temp) + "°C")
-        data.append("Rücklauf: " + str(return_temp) + "°C")
-        data.append("Leistung: " + str(power) + " W")
-        data.append("Durchfluss: " + str(round(flow*1000,2)) + " l/h")
-
-        db.write("now", "VerbrauchHeizungEg", max(energy))
-        logger("Energieverbrauch: " + str(max(energy)/1e6) + "MWh", logging)
-
-        #db.close()
-
-
-    #except OSError:
-        #do_char_dev(args)
-
-
+    db.write("now", "VerbrauchHeizungEg", max(energy))
+    logging.info("Energieverbrauch: " + str(max(energy)/1e6) + "MWh")
 
